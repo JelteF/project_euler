fa3afd96c27d662b2c20fa25d75d27a720911a6f
Jelte Fennema <jelte12345@gmail.com>
2012-12-16 19:37:30 +0100

    Add primes up to method to PrimeList

--- python/utils/my_math.py
+++ python/utils/my_math.py
@@ -1,9 +1,23 @@
 from math import sqrt
+from bisect import bisect
 
 class PrimeList:
     primes = [2, 3]
 
+    def primes_up_to(self, max_prime):
+        """ Return all the primes up to (inclusive) a certain value """
+
+        if self.primes[-1] > max_prime:
+            return self.primes[:bisect(self.primes, max_prime)]
+
+        while self.primes[-1] < max_prime:
+            self.find_next_prime()
+
+        return self.primes[:-1]
+
     def find_next_prime(self):
+        """ Find the next prime and add it to the primes list """
+
         i = self.primes[-1]
         while True:
             i += 2
