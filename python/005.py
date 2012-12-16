62875abc38d0628112c52abf27aedcd767446bfa
Jelte Fennema <jelte12345@gmail.com>
2012-12-16 21:43:50 +0100

    Add comments and better fitting numbers

--- python/005.py
+++ python/005.py
@@ -2,10 +2,22 @@ import time
 from utils.my_math import PrimeList
 from operator import mul
 
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+"""
+
+
 def divisible_by_all_lower(max_divisor):
+    """ Return the number that is divisible by all numbers lower than the
+        max_divisor
+    """
     smallest = 1
     pl = PrimeList()
-    prime_divisors = pl.primes_up_to(max_divisor + 1)
+    prime_divisors = pl.primes_up_to(max_divisor)
 
     for p in prime_divisors:
         temp = p
@@ -17,6 +29,6 @@ def divisible_by_all_lower(max_divisor):
 
 if __name__ == '__main__':
     start = time.time()
-    answer = divisible_by_all_lower(20)
+    answer = divisible_by_all_lower(21)
     print time.time() - start
     print answer
