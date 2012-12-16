262e8973984b3d976e3cc04a3936e9bbe9e997f2
Jelte Fennema <jelte12345@gmail.com>
2012-12-16 23:25:49 +0100

    Add comment and use new main function

--- python/010.py
+++ python/010.py
@@ -1,11 +1,16 @@
 from utils.my_math import Prime
-from time import time
+from utils.main import main
 
-start = time()
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-pl = Prime()
-primes = pl.up_to(2000000)
+Find the sum of all the primes below two million.
+"""
 
-print time() - start
+def sum_of_primes_up_to(n):
+    pl = Prime()
+    return sum(pl.up_to(n))
 
-print sum(primes)
+
+if __name__ == '__main__':
+    main(sum_of_primes_up_to, 2000000)
