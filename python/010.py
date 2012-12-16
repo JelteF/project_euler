93e129813c234fd6dac4ec592ceeedc02ae6ac7d
Jelte Fennema <jelte12345@gmail.com>
2012-12-16 17:41:47 +0100

    Used the utils to do this one more clean

--- python/010.py
+++ python/010.py
@@ -1,28 +1,10 @@
-import math
+from utils.my_math import PrimeList
 
-def find_next_prime(primeList):
-    i = primeList[-1]
-    while True:
-        i += 2
-        for p in primeList:
-            if p>math.sqrt(i):
-                primeList.append(i)
-                return
-            if i % p == 0:
-                break
-        else:
-            primeList.append(i)
-            return
+prime_list = PrimeList()
+primes = prime_list.primes
 
 
-primes = [2, 3]
-primeSum = 2
-counter = 0
 while primes[-1] < 2000000:
-    if counter == 1000:
-        print primes[-1]
-        counter = 0
-    primeSum += primes[-1]
-    find_next_prime(primes)
-    counter += 1
-print primeSum
+    prime_list.find_next_prime()
+
+print sum(primes[0:-1])
