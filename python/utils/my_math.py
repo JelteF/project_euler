ee340b1af007e8626e024cb8be21ce739ec9ae6e
Jelte Fennema <jelte12345@gmail.com>
2012-12-16 22:55:20 +0100

    Add a parent class ordered sequence, so some functions can be reused for other sequences

--- python/utils/my_math.py
+++ python/utils/my_math.py
@@ -1,19 +1,29 @@
 from math import sqrt
 from bisect import bisect
 
-class Prime:
-    primes = [2, 3]
 
-    def primes_up_to(self, max_prime):
-        """ Return all the primes up to (exclusive) a certain value """
+class OrderedSequence:
+    sequence = []
 
-        if self.primes[-1] > max_prime:
-            return self.primes[:bisect(self.primes, max_prime)]
+    def up_to(self, max_num):
+        """ Return all the numbers in the sequence up to (exclusive) a certain
+            value
+        """
+        if self.sequence[-1] > max_num:
+            return self.sequence[:bisect(self.sequence, max_num)]
 
-        while self.primes[-1] < max_prime:
-            self.find_next_prime()
+        while self.sequence[-1] < max_num:
+            self.find_next()
 
-        return self.primes[:-1]
+        return self.sequence[:-1]
+
+
+    def find_next(self):
+        sequence.append(0)
+
+
+class Prime(OrderedSequence):
+    sequence = [2, 3]
 
     def get_prime_devisors(n):
         """ Return a list of all the devisors """
@@ -21,7 +31,7 @@ class Prime:
         if self.is_prime(n):
             return [n]
 
-        for p in self.primes:
+        for p in self.sequence:
             while n % p == 0:
                 n /= p
                 devisors.append(p)
@@ -29,31 +39,31 @@ class Prime:
             if n == 1:
                 return devisors
 
-    def find_next_prime(self):
+    def find_next(self):
         """ Find the next prime and add it to the primes list """
 
-        i = self.primes[-1]
+        i = self.sequence[-1]
         while True:
             i += 2
             if self.is_prime(i):
-                self.primes.append(i)
+                self.sequence.append(i)
                 return
 
     def is_prime(self, possible_prime):
         """ Return a boolean value to see if a number is a prime """
 
         # Calculate new primes to do sufficient checking
-        while sqrt(possible_prime) > self.primes[-1]:
-            self.find_next_prime()
+        while sqrt(possible_prime) > self.sequence[-1]:
+            self.find_next()
 
         # Check to see if it could alread be in the list
-        if possible_prime <= self.primes[-1]:
-            if possible_prime in self.primes:
+        if possible_prime <= self.sequence[-1]:
+            if possible_prime in self.sequence:
                 return True
             return False
 
         # Check against existing primes
-        for p in self.primes:
+        for p in self.sequence:
             if possible_prime % p == 0:
                 return False
 
