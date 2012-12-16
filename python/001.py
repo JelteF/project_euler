5ecbc9bbfc5be598f63e1de6645e6c2869ac937a
Jelte Fennema <jelte12345@gmail.com>
2012-12-17 00:50:06 +0100

    Add new running convention to 001

--- python/001.py
+++ python/001.py
@@ -1,29 +1,25 @@
-import time
+from utils.my_math import Prime
+from utils.main import main
 
 """ Add all the natural numbers below one thousand
 that are multiples of 3 or 5.
 """
 
-def using_union():
-    three = range(3, 1000, 3)
-    five = range(5, 1000, 5)
+def using_union(maximum):
+    three = range(3, maximum, 3)
+    five = range(5, maximum, 5)
     return sum(set().union(three, five))
 
-def using_iteration():
+def using_iteration(maximum):
     sum_ = 0
 
-    for i in range(3, 1000, 1):
+    for i in range(3, maximum, 1):
         if i % 3 == 0 or i % 5 == 0:
             sum_ += i
 
     return sum_
 
-start = time.time()
-sum_ = using_union()
-print (time.time() - start)
-print sum_
 
-start = time.time()
-sum_ = using_iteration()
-print (time.time() - start)
-print sum_
+if __name__ == '__main__':
+    main(using_union, 1000)
+    main(using_iteration, 1000)
