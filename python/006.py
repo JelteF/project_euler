f24c4e8283757b612c0c9a48813a748c54c852be
Jelte Fennema <jelte12345@gmail.com>
2012-12-17 01:18:38 +0100

    Use a much more pythonic algorithm for 006

--- python/006.py
+++ python/006.py
@@ -1,8 +1,23 @@
-one = 0
-for a in range(1, 101):
-    one += a**2
-two = 0
-for a in range(1, 101):
-    two += a
-two = two**2
-print two - one
+from utils.main import main
+
+"""
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+def diff_between_sum_of_squares(n):
+    sum_of_squares = sum(map(lambda x: x**2, range(n)))
+    square_of_sum = sum(range(n))**2
+
+    return square_of_sum - sum_of_squares
+
+
+if __name__ == '__main__':
+    main(diff_between_sum_of_squares, 101)
