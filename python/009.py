9497a6d9e5ad36e24ef1a8cecf59419d0441ea15
Jelte Fennema <jelte12345@gmail.com>
2012-12-17 03:38:13 +0100

    Speed up the algorithm a bit and use the new notation

--- python/009.py
+++ python/009.py
@@ -1,8 +1,22 @@
-for a in range(1, 1000):
-    for b in range(a, 1000-a):
-        c = 1000 - a - b
-        if (a**2 + b**2) == c**2:
-            print a
-            print b
-            print c
-            print a*b*c
+from utils.main import main
+
+"""
+A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+
+def product_of_triplet_with_sum(sum_):
+    for a in range(1, sum_ / 3):
+        for b in range(a, (sum_ / 3)*2 - a):
+            c = sum_ - a - b
+            if (a**2 + b**2) == c**2:
+                return a*b*c
+
+
+if __name__ == '__main__':
+    main(product_of_triplet_with_sum, 1000)
