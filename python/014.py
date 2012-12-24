802ea5710e029d9f94a31562565936a370999923
Jelte Fennema <jelte12345@gmail.com>
2012-12-24 22:53:34 +0100

    Use a more broad implementation, that can return the amount of steps of any of the nodes that have been calculated

--- python/014.py
+++ python/014.py
@@ -1,20 +1,45 @@
-sequenceLength = [0]
-maximum = 0
-highest = 0
-for a in range(1, 1000000):
-    n = a
-    sequenceLength.append(1)
-    while n > 1:
-        if n % 2 == 0:
-            n /= 2
-            sequenceLength[a] += 1
-        else:
-            n = 3*n+1
-            sequenceLength[a] += 1
-        if len(sequenceLength) > n:
-            sequenceLength[a] += sequenceLength[n] - 1
-            break
-    if maximum < sequenceLength[a]:
-        maximum = sequenceLength[a]
-        highest = a
-print highest
+from utils.main import main
+
+"""
+The following iterative sequence is defined for the set of positive integers:
+
+n  n/2 (n is even) n  3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13  40  20  10  5  16  8  4  2  1 It can be seen that this sequence (starting
+at 13 and finishing at 1) contains 10 terms. Although it has not been proved
+yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+"""
+
+
+def next_collatz_number(n):
+    if n % 2 == 0:
+        return n/2
+    return 3*n + 1
+
+
+def longest_collatz_chain(n):
+    collatz = {1:0}
+    longest = 1
+    for a in xrange(2, n):
+        new = [a]
+        while new[-1] not in collatz:
+            new.append(next_collatz_number(new[-1]))
+
+        print a, new
+        for i, val in enumerate(new[:-1]):
+            collatz[val] = collatz[new[-1]] + len(new) - i - 1
+
+        if collatz[a] > collatz[longest]:
+            longest = a
+
+    return longest
+
+
+if __name__ == '__main__':
+    main(longest_collatz_chain, 100)
