598009fa1fa9b36b0b9d5ed3684653f2c0581375
Jelte Fennema <jelte12345@gmail.com>
2012-12-24 23:45:51 +0100

    Fix copy paste typo in the description

--- python/016.py
+++ python/016.py
@@ -3,7 +3,7 @@ from utils.main import main
 """
 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 """
 
 def find_sum_of_digits(number):
