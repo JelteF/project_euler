9008e1f2154efa38ce830966eef54a3d243e797b
Jelte Fennema <jelte12345@gmail.com>
2012-12-24 23:05:43 +0100

    Used map to create a nice one liner

--- python/016.py
+++ python/016.py
@@ -1,5 +1,14 @@
-number = str(2**1000)
-summ = 0
-for x in number:
-    summ += int(x)
-print summ
+from utils.main import main
+
+"""
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+"""
+
+def find_sum_of_digits(number):
+    return sum(map(int, str(number)))
+
+
+if __name__ == '__main__':
+    main(find_sum_of_digits, 2**1000)
