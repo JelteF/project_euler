a0b3bf6d88933d3198b703c6c7093699bf84ad21
Jelte Fennema <jelte12345@gmail.com>
2012-12-24 21:34:13 +0100

    Clean up the code and use the utils module

--- python/012.py
+++ python/012.py
@@ -1,54 +1,22 @@
-import math
+from utils.my_math import Prime, Triangle
+from utils.main import main
 
-def find_next_triangle(triangleList):
-    triangleList.append(triangleList[-1]+len(triangleList)+1)
+def get_amount_of_divisors(pl, n):
+    factors, frequencies = pl.get_prime_factor_frequency(n)
+    possible_combinations = 1
+    for f in frequencies:
+        possible_combinations *= f + 1
 
-def find_next_prime(primeList):
-    i = primeList[-1]
-    while True:
-        i += 2
-        for p in primeList:
-            if p>math.sqrt(i):
-                primeList.append(i)
-                return
-            if i % p == 0:
-                break
-        else:
-            primeList.append(i)
-            return
+    return possible_combinations
 
 
-def primefactors(number, primes):
-    factors = [[],[]]
-    for a in primes:
-        if number % a == 0:
-            factors[0].append(a)
-            factors[1].append(0)
-            while number % a == 0:
-                factors[1][-1] += 1
-                number /= a
-    find_next_prime(primes)
-    while primes[-1] <= number:
-        if number % primes[-1] == 0:
-            factors[0].append(a)
-            factors[1].append(0)
-            while number % primes[-1] == 0:
-                number /= primes[-1]
-                factors[-1] += 1
-        else:
-            find_next_prime(primes)
-    return factors
+def first_triangle_divisors(n):
+    tri = Triangle()
+    pl = Prime()
+    while get_amount_of_divisors(pl, tri.sequence[-1]) < n:
+        tri.find_next()
+    return tri.sequence[-1]
 
 
-primes = [2,3]
-triangles = [1]
-divisors = 0
-counter = 0
-while divisors <500:
-    divisors = 1
-    find_next_triangle(triangles)
-    factors = primefactors(triangles[-1], primes)
-    for a in factors[1]:
-        divisors *= a+1
-    counter += 1
-print triangles[-1]
+if __name__ == '__main__':
+    main(first_triangle_divisors, 500)
